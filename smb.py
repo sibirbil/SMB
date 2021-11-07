7a285edb56f6e10d2d905457b6768d012935208c
Ozgur Martin <ozgurmartin@gmail.com>
2021-11-07 16:55:53 +0300

    Initial commit

---
+++ smb.py
@@ -0,0 +1,356 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import torch
+from torch.optim import Optimizer
+from typing import Iterable
+
+
+##############################################################################
+# Function definitions (taken from https://github.com/IssamLaradji/sls/blob/master/sls/utils.py)
+##############################################################################
+
+
+
+def get_grad_list(params):
+    return [p.grad for p in params]
+
+def compute_grad_norm(grad_list):
+    grad_norm = 0.
+    for g in grad_list:
+        if g is None:
+            continue
+        grad_norm += torch.sum(torch.mul(g, g))
+    grad_norm = torch.sqrt(grad_norm)
+    return grad_norm
+
+#def get_lr(state, params):
+#    return max([state[p]['lr'] for p in params])
+
+
+##############################################################################
+# SMB Optimizer
+##############################################################################
+
+class SMB(Optimizer):
+    def __init__(
+        self,
+        params: Iterable[torch.nn.parameter.Parameter],
+        lr: float = 1,
+        eta: float = 0.99,
+        c: float = 0.1,
+        independent_batch = False,
+        n_batches_per_epoch: int = 500,
+        autoschedule = False,
+        gamma: float = 0.05, 
+        beta: float = 0.9,
+        lr_max: float = 20
+    ):
+        defaults = dict(lr=lr, 
+                        eta=eta, 
+                        c=c, 
+                        independent_batch=independent_batch, 
+                        n_batches_per_epoch=n_batches_per_epoch,
+                        autoschedule=autoschedule,
+                        gamma=gamma,
+                        beta=beta,
+                        lr_max=lr_max
+                        )
+        super().__init__(params, defaults)
+        
+        
+    def step(self, closure):
+        
+        independent_batch = False
+        
+        for group in self.param_groups:
+            
+             independent_batch =  group["independent_batch"]
+            
+        if independent_batch:
+            
+            return self.step_smbi(closure)
+        
+        else:
+            
+            return self.step_smb(closure)
+            
+        
+    def step_smb(self, closure):
+        
+        if len(self.state) == 0:
+            self.state['step'] = 0
+            self.state['model_step_list'] = []
+            
+        loss = closure()
+        loss.backward()
+        
+        for group in self.param_groups:
+            
+            lr =  group["lr"]  
+            eta =  group["eta"]
+            c = group["c"]
+            
+            params = group["params"]
+            
+            # Auto-scheduling parameters
+            gamma = group["gamma"]
+            beta = group["beta"]
+            lr_max = group["lr_max"]
+            
+            
+            grad_current = get_grad_list(params)
+            grad_norm = compute_grad_norm(grad_current)
+            
+            cond = loss.item() - c * lr * grad_norm.pow(2).item()
+            
+            
+            for p in params:
+                
+                state = self.state[p]
+                
+                # State initialization
+                if len(state) == 0:
+                    state["grad_old"] = torch.zeros_like(p.grad)
+                    state["s_old"] = torch.zeros_like(p.data)
+                    
+                
+                if p.grad is None:
+                    continue    
+                    
+                grad = p.grad.data
+                state["grad_old"] = grad.clone().detach()
+                
+                if grad.is_sparse:
+                    raise RuntimeError("SMB does not support sparse gradients")
+                
+                s_new = grad.mul(-lr)
+                state["s_old"] = s_new.clone().detach()
+                
+                p.data.add_(s_new, alpha=1.0)
+                
+            
+            loss_next = closure()
+            
+            
+            if grad_norm >= 1e-8: 
+
+                if loss_next.item() <= cond:
+                    
+                    self.state["model_step_list"].append(0)
+                    
+                else:
+                        
+                    loss_next.backward()
+    
+                    for p in group["params"]:
+    
+                        state = self.state[p]
+    
+                        grad = state["grad_old"]
+                        grad_t = p.grad.data
+    
+                        s_old = state["s_old"]
+    
+                        g = torch.flatten(grad)
+                        gt = torch.flatten(grad_t)
+                        s = torch.flatten(s_old)
+    
+                        sg = torch.dot(s,g)  # (v6)
+                        sgt = torch.dot(s,gt)
+    
+                        y_t = grad_t.sub(grad, alpha=1.0) # y^k_t = g_t - g
+    
+                        y = torch.flatten(y_t)
+                        ys = sgt - sg       # v1 
+                        ss = torch.dot(s,s) # v2
+                        yy = torch.dot(y,y) # v3
+                        yg = torch.dot(y,g) # v4
+                        gg = torch.dot(g,g) # v5
+    
+                        sigma = 0.5*(torch.sqrt(ss)*(torch.sqrt(yy)+torch.sqrt(gg)/eta)-ys)
+                        theta = (ys + 2.0*sigma)**2.0 - ss*yy
+    
+                        cg = -ss/(2.0*sigma)                       # cg(sigma)
+                        cs = cg/theta*(-(ys + 2.0*sigma)*yg+yy*sg) # cs(sigma)
+                        cy = cg/theta*(-(ys + 2.0*sigma)*sg+ss*yg) # cy(sigma)
+    
+                        s_new = s_old.mul(cs).add(grad, alpha=cg).add(y_t, alpha=cy)
+                            
+                        p.data.sub_(s_old, alpha=1.0).add_(s_new, alpha=1.0)    
+                      
+                    self.state["model_step_list"].append(1)
+            
+            else:
+                
+                self.state["model_step_list"].append(0)
+                
+            #Auto-scheduling
+            if group["autoschedule"] and len(self.state['model_step_list']) >= group["n_batches_per_epoch"]:
+                if sum(self.state['model_step_list']) / len(self.state['model_step_list']) > gamma:
+                    group["lr"] *= beta
+                elif group["lr"]/beta <= lr_max:
+                    group["lr"] /= beta
+                self.state['model_step_list'] = []
+                
+                     
+        self.state['step'] += 1
+        
+                
+        return loss
+    
+    
+    def step_smbi(self, closure):
+        
+        if len(self.state) == 0:
+            self.state['step'] = 0
+            self.state['model_step'] = False
+            self.state['model_step_list'] = []
+            
+        loss = closure()
+        loss.backward()
+            
+        
+        for group in self.param_groups:
+            
+            lr =  group["lr"]  
+            eta =  group["eta"]
+            c = group["c"]
+            
+            params = group["params"]
+            
+            # Auto-scheduling parameters
+            gamma = group["gamma"]
+            beta = group["beta"]
+            lr_max = group["lr_max"]
+            
+            
+            grad_current = get_grad_list(params)
+            grad_norm = compute_grad_norm(grad_current)
+            
+            cond = loss.item() - c * lr * grad_norm.pow(2).item()
+            
+            
+            if not self.state['model_step']:
+                
+                self.state["model_step_list"].append(0)
+
+                for p in params:
+
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state["grad_prev"] = torch.zeros_like(p.grad)
+                        state["grad_old"] = torch.zeros_like(p.grad)
+                        state["s_old"] = torch.zeros_like(p.data)
+
+                    if p.grad is None:
+                        continue    
+
+                    grad = p.grad.data
+                    state["grad_prev"] = grad.clone().detach()
+
+                    if grad.is_sparse:
+                        raise RuntimeError("SMB does not support sparse gradients")
+
+                    s_new = grad.mul(-lr)
+                    state["s_old"] = s_new.clone().detach()
+
+                    p.data.add_(s_new, alpha=1.0)
+
+                loss_next = closure()
+        
+
+                if grad_norm >= 1e-8: 
+
+                    if loss_next.item() > cond:
+                        
+                        for p in params:
+                            state = self.state[p]
+                            p.data.sub_(state["s_old"], alpha=1.0)
+
+                        self.state['model_step'] = True
+                    
+             
+            else:
+                
+                self.state["model_step_list"].append(1)
+                
+                for p in params:
+
+                    state = self.state[p]
+
+                    if p.grad is None:
+                        continue    
+
+                    grad = p.grad.data
+                    state["grad_old"] = grad.clone().detach()
+
+                    if grad.is_sparse:
+                        raise RuntimeError("SMB does not support sparse gradients")
+
+                    s_new = grad.mul(-lr)
+                    state["s_old"] = s_new.clone().detach()
+
+                    p.data.add_(s_new, alpha=1.0)
+
+                loss_next = closure()
+                loss_next.backward()
+
+                for p in group["params"]:
+
+                    state = self.state[p]
+                    
+                    grad_prev = state["grad_prev"]
+                    grad = state["grad_old"]
+                    grad_t = p.grad.data
+                    s_old = state["s_old"]
+                    
+                    y_t = grad_t.sub(grad, alpha=1.0) # y^k_t = g_t - g
+                    
+                    g_prev = torch.flatten(grad_prev)
+                    g = torch.flatten(grad)
+                    gt = torch.flatten(grad_t)
+                    s = torch.flatten(s_old)
+                    y = torch.flatten(y_t)
+
+                    
+                    ys = torch.dot(y,s)      # v1
+                    ss = torch.dot(s,s)      # v2
+                    yy = torch.dot(y,y)      # v3
+                    yg = torch.dot(y,g_prev) # v4
+                    gg = torch.dot(g,g)      # v5
+                    sg = torch.dot(s,g_prev) # (v6)
+
+                    sigma = 0.5*(torch.sqrt(ss)*(torch.sqrt(yy)+torch.sqrt(gg)/eta)-ys)
+                    theta = (ys + 2.0*sigma)**2.0 - ss*yy
+
+                    cg = -ss/(2.0*sigma)                       # cg(sigma)
+                    cs = cg/theta*(-(ys + 2.0*sigma)*yg+yy*sg) # cs(sigma)
+                    cy = cg/theta*(-(ys + 2.0*sigma)*sg+ss*yg) # cy(sigma)
+
+                    s_new = s_old.mul(cs).add(grad_prev, alpha=cg).add(y_t, alpha=cy)
+                        
+                    p.data.sub_(state["s_old"], alpha=1.0).add_(s_new, alpha=1.0)
+                 
+                self.state['model_step'] = False
+                
+            #Auto-scheduling
+            if group["autoschedule"] and len(self.state['model_step_list']) >= group["n_batches_per_epoch"]:
+                if sum(self.state['model_step_list']) / len(self.state['model_step_list']) > gamma/2:
+                    group["lr"] *= beta
+                elif group["lr"]/beta <= lr_max:
+                    group["lr"] /= beta
+                self.state['model_step_list'] = []
+                
+        self.state['step'] += 1
+                
+        return loss
+    
+    
+    
+
+
+
---
+++ train.py
@@ -0,0 +1,429 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import torch
+import torch.optim as optim
+
+import time
+import datetime
+import numpy as np
+import json
+
+
+import sls
+import smb
+from utils import *
+
+##############################################################################
+# OPTIONS
+##############################################################################
+
+
+# Epochs to train for
+epochs = 200
+
+# Dataset-Model
+TrainOptions = {1:('mnist', 'mlp'), 
+                2:('cifar10', 'resnet34_10'), 
+                3:('cifar10', 'densenet10'), 
+                4:('cifar100', 'resnet34_100'), 
+                5:('cifar100', 'densenet10')
+                }
+dataset_name, model_name = TrainOptions[1]
+
+
+# Batch Size
+batch_size = 128
+
+
+##############################################################################
+
+# Check if GPU is available
+use_GPU = torch.cuda.is_available()
+
+
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+    
+
+# Get Dataset
+train_set, test_set, train_loader = get_dataset(dataset_name, batch_size)
+n_batches_per_epoch = len(train_loader)
+
+ 
+# Get Model
+model = get_model(model_name)
+if use_GPU:
+    model.cuda()
+
+
+##############################################################################
+# Train with SMB optimizer
+##############################################################################
+
+independent_batch = False
+autoschedule = False
+
+opt_out = {}
+
+if independent_batch:
+    opt_out.update({'name':'SMBi'})
+else:
+    opt_out.update({'name':'SMB'})
+
+opt_out.update({'independent_batch':independent_batch, 
+           'autoschedule':autoschedule, 
+           'gamma':0.05, 
+           'beta':0.9, 
+           'lr':1, 
+           'c':0.1, 
+           'eta':0.99, 
+           'data':dataset_name, 
+           'model':model_name, 
+           })
+ 
+ 
+# loss function
+criterion = softmax_loss
+ 
+optimizer = smb.SMB(model.parameters(), 
+                lr=opt_out['lr'], 
+                c=opt_out['c'], 
+                eta=opt_out['eta'], 
+                independent_batch=opt_out['independent_batch'], 
+                autoschedule=opt_out['autoschedule'],
+                n_batches_per_epoch=n_batches_per_epoch)
+
+print('\n' + 'Starting to train with {} optimizer: For {} epochs'.format(opt_out['name'], epochs))
+
+train_loss_list = []
+test_acc_list = []
+run_time_list = []
+
+
+for epoch in range(1, epochs+1):
+    
+    step_type = []
+        
+    begin = time.time()
+    
+    # training steps
+    model.train()
+    
+    for batch_index, (data, target) in enumerate(train_loader):
+        
+        # moves tensors to GPU if available
+        if use_GPU:
+            data, target = data.cuda(), target.cuda() 
+            
+        # create loss closure for smb algorithm
+        def closure():
+            optimizer.zero_grad()
+            loss = criterion(model, data, target)
+            return loss
+        
+        # forward pass
+        loss = optimizer.step(closure=closure)
+        
+    end = time.time()
+        
+    train_loss = compute_loss(model, train_set)
+    test_acc = compute_accuracy(model, test_set)
+        
+    train_loss_list.append(train_loss)
+    test_acc_list.append(test_acc)
+    run_time_list.append(end-begin)
+        
+    # Display loss statistics
+    print(f'Epoch: {epoch}   -   Training Loss: {round(train_loss, 6)}  -  Test Accuracy: {round(test_acc, 6)}  -  Time: {round(end-begin, 2)}')
+
+    
+opt_out.update({'train_loss':train_loss_list,
+                 'test_acc':test_acc_list,
+                 'run_time':run_time_list,
+                })
+
+now = datetime.datetime.now() # current date and time
+date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+
+filename = "{}_{}_{}_{}.json".format(opt_out['name'],
+                                             opt_out['data'], 
+                                             opt_out['model'],
+                                             date_time
+                                             )
+
+with open(filename, 'w') as f:
+    json.dump(opt_out, f)
+
+ 
+
+##############################################################################
+# Train with SLS optimizer
+##############################################################################
+
+
+opt_out = {'name':'SLS', 
+           'lr':1, 
+           'c':0.1, 
+           'reset_option':1, 
+           'data':dataset_name, 
+           'model':model_name, 
+           }
+
+ 
+# loss function
+criterion = softmax_loss
+
+
+optimizer = sls.Sls(model.parameters(), 
+                    init_step_size=opt_out['lr'], 
+                    reset_option=opt_out['reset_option'], 
+                    c=opt_out['c'], 
+                    n_batches_per_epoch=n_batches_per_epoch
+                   )
+
+print('\n' + 'Starting to train with {} optimizer: For {} epochs'.format(opt_out['name'], epochs))
+
+
+train_loss_list = []
+train_iter_loss_list = []
+test_acc_list = []
+run_time_list = []
+
+loss = None
+
+for epoch in range(1, epochs+1):
+        
+    begin = time.time()
+    
+    # training steps
+    model.train()
+    
+    for batch_index, (data, target) in enumerate(train_loader):
+        
+        # moves tensors to GPU
+        if use_GPU:
+            data, target = data.cuda(), target.cuda() 
+            
+        # create loss closure for sls algorithm
+        closure = lambda :  criterion(model, data, target)  
+        # clears gradients
+        optimizer.zero_grad()
+        
+        loss = optimizer.step(closure=closure)
+        
+        train_iter_loss_list.append(loss.item())
+        
+    end = time.time()
+    
+    train_loss = compute_loss(model, train_set)
+    test_acc = compute_accuracy(model, test_set)
+        
+    train_loss_list.append(train_loss)
+    test_acc_list.append(test_acc)
+    run_time_list.append(end-begin)
+        
+    # Display loss statistics
+    print(f'Epoch: {epoch}   -   Training Loss: {round(train_loss, 6)}  -  Test Accuracy: {round(test_acc, 6)}  -  Time: {round(end-begin, 2)}')
+    
+    #print(epoch, end=' ')
+    
+opt_out.update({'train_loss':train_loss_list,
+                 'test_acc':test_acc_list,
+                 'run_time':run_time_list,
+                 'train_iter_loss':train_iter_loss_list,
+                })
+
+
+now = datetime.datetime.now() # current date and time
+date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+
+filename = "{}_{}_{}_{}.json".format(opt_out['name'],
+                                             opt_out['data'], 
+                                             opt_out['model'],
+                                             date_time
+                                             )
+
+with open(filename, 'w') as f:
+    json.dump(opt_out, f)
+    
+
+
+
+
+
+
+
+##############################################################################
+# Train with Adam optimizer
+##############################################################################
+
+opt_out = {'name':'Adam', 
+           'lr':0.001, 
+           'data':dataset_name, 
+           'model':model_name,
+           } 
+ 
+# loss function
+criterion = softmax_loss
+
+# optimizer
+optimizer = optim.Adam(model.parameters(), lr = opt_out['lr'])
+
+
+print('\n' + 'Starting to train with {} optimizer: For {} epochs'.format(opt_out['name'], epochs))
+
+
+train_loss_list = []
+test_acc_list = []
+run_time_list = []
+    
+
+for epoch in range(1, epochs+1):
+        
+    begin = time.time()
+
+    # training steps
+    model.train()
+    for batch_index, (data, target) in enumerate(train_loader):            
+            
+        # moves tensors to GPU
+        if use_GPU:
+            data, target = data.cuda(), target.cuda()     
+        # clears gradients
+        optimizer.zero_grad()
+        # loss in batch
+        loss = criterion(model, data, target)
+        # backward pass for loss gradient
+        loss.backward()
+            
+            
+        # update paremeters
+        optimizer.step()
+            
+    end = time.time()
+    
+    
+    # Calculate metrics
+    train_loss = compute_loss(model, train_set)
+    test_acc = compute_accuracy(model, test_set)
+    
+    train_loss_list.append(train_loss)
+    test_acc_list.append(test_acc)
+    run_time_list.append(end-begin)
+        
+    # Display loss statistics
+    print(f'Epoch: {epoch}   -   Training Loss: {round(train_loss, 6)}   -   Test Accuracy: {round(test_acc, 6)}  -  Time: {round(end-begin, 2)}')
+
+
+opt_out.update({'train_loss':train_loss_list,
+                 'test_acc':test_acc_list,
+                 'run_time':run_time_list,
+                })
+
+now = datetime.datetime.now() # current date and time
+date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+
+filename = "{}_{}_{}_{}.json".format(opt_out['name'],
+                                             opt_out['data'], 
+                                             opt_out['model'],
+                                             date_time
+                                             )
+
+with open(filename, 'w') as f:
+    json.dump(opt_out, f)
+
+
+
+##############################################################################
+# Train with SGD optimizer
+##############################################################################
+
+opt_out = {'name':'SGD', 
+           'lr':0.1, 
+           'data':dataset_name, 
+           'model':model_name,
+           }
+ 
+# loss function
+criterion = softmax_loss
+
+# optimizer
+optimizer = optim.SGD(model.parameters(), lr = opt_out['lr'])
+
+print('\n' + 'Starting to train with {} optimizer: For {} epochs'.format(opt_out['name'], epochs))
+
+
+train_loss_list = []
+test_acc_list = []
+run_time_list = []
+    
+
+for epoch in range(1, epochs+1):
+        
+    begin = time.time()
+
+    # training steps
+    model.train()
+    for batch_index, (data, target) in enumerate(train_loader):            
+            
+        # moves tensors to GPU
+        if use_GPU:
+            data, target = data.cuda(), target.cuda()     
+        # clears gradients
+        optimizer.zero_grad()
+        # loss in batch
+        loss = criterion(model, data, target)
+        # backward pass for loss gradient
+        loss.backward()
+            
+            
+        # update paremeters
+        optimizer.step()
+            
+    end = time.time()
+    
+    
+    # Calculate metrics
+    train_loss = compute_loss(model, train_set)
+    test_acc = compute_accuracy(model, test_set)
+    
+    train_loss_list.append(train_loss)
+    test_acc_list.append(test_acc)
+    run_time_list.append(end-begin)
+        
+    # Display loss statistics
+    print(f'Epoch: {epoch}   -   Training Loss: {round(train_loss, 6)}   -   Test Accuracy: {round(test_acc, 6)}  -  Time: {round(end-begin, 2)}')
+
+
+opt_out.update({'train_loss':train_loss_list,
+                 'test_acc':test_acc_list,
+                 'run_time':run_time_list,
+                })
+
+now = datetime.datetime.now() # current date and time
+date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+
+filename = "{}_{}_{}_{}.json".format(opt_out['name'],
+                                             opt_out['data'], 
+                                             opt_out['model'],
+                                             date_time
+                                             )
+
+with open(filename, 'w') as f:
+    json.dump(opt_out, f)
+    
+    
+    
+
+ 
+
+
+    
